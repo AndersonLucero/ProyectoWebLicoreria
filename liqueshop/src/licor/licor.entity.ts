74d1847493e3f1cc5d4d031c6ca29cfdcacc6d56
anderson Lucero <michael.lucero@epn.edu.ec>
2020-09-30 10:07:47 -0500

    licor v2

--- liqueshop/src/licor/licor.entity.ts
+++ liqueshop/src/licor/licor.entity.ts
@@ -1,3 +1,34 @@
+import {Column, Entity, PrimaryGeneratedColumn} from "typeorm";
+
+@Entity('licor')
+
 export class LicorEntity{
+    @PrimaryGeneratedColumn({
+        name: 'idLicor'
+    })
+    idlicor: number
+
+    @Column({
+        name: 'nombreLicor',
+        type: 'varchar',
+        length: '50',
+        nullable: false
+    })
+    nombreLicor: string
+
+    @Column({
+        name: 'detallesLicor',
+        type: 'varchar',
+        length: '100',
+        nullable: false
+    })
+    detallesLicor: string
+
+    @Column({
+        name: 'cantidadLicor',
+        type: 'varchar',
+        nullable: false
+    })
+    cantidadLicor: number
 
 }
